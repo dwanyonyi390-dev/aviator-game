c025115e1351537d096d0395753ac47da3b92433
dwanyonyi390-dev <dwanyonyi390@gmail.com>
2026-08-22 20:32:49 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    balance: {
+        type: Number,
+        default: 1000,
+        min: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Hash password before saving
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// Compare password method
+UserSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
