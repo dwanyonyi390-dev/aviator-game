7ee08e4fce44d7b9289e18e34eee46aae4238f80
dwanyonyi390-dev <dwanyonyi390@gmail.com>
2026-08-22 20:33:32 +0300

    Update GameHistory.js

--- models/GameHistory.js
+++ models/GameHistory.js
@@ -1,1 +1,22 @@
+const mongoose = require('mongoose');
 
+const GameHistorySchema = new mongoose.Schema({
+    roundId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    crashPoint: {
+        type: Number,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Index for faster queries
+GameHistorySchema.index({ roundId: -1 });
+
+module.exports = mongoose.model('GameHistory', GameHistorySchema);
